e04cec8ced5c6e4ed45ec081d3f623c9756b43fb
Dmitry Mitichenko <dmitry.mitichenko@gmail.com>
2019-11-16 13:35:50 +0300

    ci(pipeline): get git version using gitversion.tool in docker

--- ci/git-version.sh
+++ ci/git-version.sh
@@ -8,17 +8,43 @@
 # > ./getver.sh -t FullSemVer
 
 type=
+url=
+branch=
+sha=
+user=
+password=
+image=rg.nl-ams.scw.cloud/askaone/gitversion:latest
 
 while [ "$1" != "" ]; do
     case $1 in
         -t | --type )           shift
                                 type=$1
                                 ;;
+        --url )                 shift
+                                url=$1
+                                ;;
+        --user )                shift
+                                user=$1
+                                ;;
+        --password )            shift
+                                password=$1
+                                ;;
+        --branch )              shift
+                                branch=$1
+                                ;;
+        --sha )              shift
+                                sha=$1
+                                ;;                     
         * )                     echo 'Wrong parameter'
                                 exit 1
     esac
     shift
 done
 
-docker run -it -v /home/frame/projects/aska.core:/src rg.nl-ams.scw.cloud/askaone/gitversion:latest \
+docker pull $image > /dev/null
+
+docker run --tmpfs /git $image /url $url /b $branch /c $sha /dynamicRepoLocation /git /u $user /p $password \
 | sed -e 's/.*{.*/{/g' -e 's/.*}.*/}/g' -e 's/.*=.*/ /g' | jq ".$type"
+
+#docker run -v $path:/src $image \
+#| sed -e 's/.*{.*/{/g' -e 's/.*}.*/}/g' -e 's/.*=.*/ /g' | jq ".$type"
