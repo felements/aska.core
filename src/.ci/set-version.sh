f928cb1a480dbbf549d268cc43ad55d08c279b55
Dmitry Mitichenko <dmitry.mitichenko@gmail.com>
2019-12-18 01:08:36 +0300

    WIP fix(ci): attempt to fix publish pipeline

--- src/.ci/set-version.sh
+++ src/.ci/set-version.sh
@@ -18,10 +18,8 @@ done
 echo "Settings projects version to $version"
 # https://stackoverflow.com/questions/9612090/how-to-loop-through-file-names-returned-by-find
 
+
 find . -name "*.csproj" -print0 | while read -d $'\0' fname; do \
-      echo "$fname" && \
-      sed -i \
-        -e "s/<Version>.*<\/Version>/<Version>$version<\/Version>/g" \
-#        -e "s/<ProjectReference.*EntityStorage\.Abstractions.*\/>/<PackageReference Include=\"Aska.Core.EntityStorage.Abstractions\" Version=\"[$version]\" \/>/g" \
-        $fname && \
-      cat $fname ; done
+  echo "$fname" && \
+  sed -i -e "s/<Version>.*<\/Version>/<Version>$version<\/Version>/g" $fname && \
+  echo "Done with '$fname'" ; done
